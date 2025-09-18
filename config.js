aa9a71ca8ea333cf56abfa1e42d8110e25186690
alongbots <aosiblings10@gmail.com>
2025-09-18 19:16:43 +0530

    Update config.js
    Signed-off-by: alongbots <aosiblings10@gmail.com>

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MAKAMESCO-MD<=>eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0lPekZHc3pzSmdVOG4rR1g3dkk1VEZWVjZYQjQ5V0laeDFMZXcrU3UxZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidjQzUHZqdlovK3d1ZkppczRpbE9jKzdkRVY5Y2YyN2lzQWZ4MEoyNVNCRT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNR213L1VKT2ttcjR6dk1iaWFiUDI3M2tRUXNTRi8yWmRDRUlsOEtXRmt3PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJubXNvU3U1R0FSSHA0bWJUdmNKS05BaEJzTkxKSmQ5ZTYzVFBBQTlEQkE0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVFZE94dnh3OTBELzJwaEQ3ck9XYnJMVVRBaGFBR2FGOTAxQnJHcDNtR2M9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjE1Y3FBR1VhaVF0dlFUdUwxWjZwNEsxamExQVpBUFlFc1VZYU1adFF2SE09In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid1B1M1hrcUFpTlZyVDlvaHdtdWlBMU9KeUVkaUgxckdsci9lNnVzV21Faz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUZyVThEWjBIVUcwS0JUVUM3ckhtK2FKTlpaWVplZWxqTVZhMlRSdW5WVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjFlQno4MTZ0aUpVTVJwTnRhRENRY1ZEaGxNOFlIcFk4VHBOd1ZiTnFNNXRSWWRyV2hMbGJLUHR2VDRLU1JLa2pHV0hqR3V0ejg2MUdLSEVJMjdIZWhBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTE5LCJhZHZTZWNyZXRLZXkiOiJOQjFYMm1sSzErelQ2YnQ0N0JQRXYwZk1jWG9jUlhtdkVNUnZadjdncFdJPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjkxNzAwNTk3MjA4NUBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiJBQzUyOTlBM0Q5M0M0RDM3REY1RTI4MTE1MzIzMjkwNSJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzU4MjAwNDEzfSx7ImtleSI6eyJyZW1vdGVKaWQiOiI5MTcwMDU5NzIwODVAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiQUM0OTIzOEI0QUM4QTBEQzMzRTI5NUYxOTE0NzExM0EifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1ODIwMDQxOX0seyJrZXkiOnsicmVtb3RlSmlkIjoiOTE3MDA1OTcyMDg1QHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IkFDNTA3NEFGNzAxM0ZEOEJGQUU0QURDRTVDQzhEODczIn0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3NTgyMDA0NDh9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IjFMVjEzRFlYIiwibWUiOnsiaWQiOiI5MTcwMDU5NzIwODU6MTJAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIyMjEyNzcxMTc3NTU1Nzk6MTJAbGlkIiwibmFtZSI6IkdvZHpfYWt1bSJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDUHFBMHZJQ0VNQ0VzTVlHR0FZZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoielpOWmxXSWVQdXJtam5QUlNQWWtWaWVDVmx4b2NIMTM2M1dEaG1mNWFTdz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiNHQrejNaSzJqMWRmeFlWRFp6N3F5UDFZdkpOVlVML04waTJmU2dFem96OHQyNForVTk3Y3JMU1NoTEFITDRxaTN0ZytwcktOUXdvSkFZMCtodTMyQ2c9PSIsImRldmljZVNpZ25hdHVyZSI6ImVRZnpxeW51YlgwR0pHa0pzLzM5QnN6Y2JmWmFhWFRES0lpY2FubTV4UHBQTmpGZklrREdBbzQ1Wjh3R3A4MXRkK1AwVDlvSHMrY3VuQ1FoSktiQmpnPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTE3MDA1OTcyMDg1OjEyQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmMyVFdaVmlIajdxNW81ejBVajJKRlluZ2xaY2FIQjlkK3QxZzRabitXa3MifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBSUlDQT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1ODIwMDM5OSwibGFzdFByb3BIYXNoIjoiQzRmSk4iLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUtKUyJ9",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DIGITEX-MD 🤍*",
 // set the auto reply massage on status reply  
@@ -29,7 +29,7 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "DIGITEX XMD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DIGITEX XMD",
+STICKER_NAME: process.env.STICKER_NAME || "ALONG",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,11 +37,11 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254748571920",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "917005972085",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DIGITEX",
+OWNER_NAME: process.env.OWNER_NAME || "akum",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© powered by DIGITEX-XMD*",
+DESCRIPTION: process.env.DESCRIPTION || "*© powered by ALONG*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/g7qjxj.png",
 // add img for alive msg
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254748571920",
+DEV: process.env.DEV || "918731810311",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
